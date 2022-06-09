ca9ca87a52d621e52adf86a5b64a548cc2395fd4
DoTheBestMayB <send1684@gmail.com>
2022-06-09 23:32:37 +0900

    build: data모듈에 rxjava3 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -12,4 +12,6 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.9.0")
+
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
 }
