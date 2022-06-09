80a036ae67eccf44cb5ccaa18e5c916e1b9657fb
DoTheBestMayB <send1684@gmail.com>
2022-06-10 00:52:26 +0900

    build: data 모듈에 okhttp3, mockwebserver 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -9,9 +9,12 @@ java {
 }
 
 dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.9.0")
+
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
 
     implementation("io.reactivex.rxjava3:rxjava:3.1.5")
 }
