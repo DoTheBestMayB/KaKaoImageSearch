ba2f839ba5bf8404dff92dd450b9db61b4be8ffc
DoTheBestMayB <send1684@gmail.com>
2022-06-10 02:23:47 +0900

    build: data 모듈에 retrofit2 rxjava adapter 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.9.3")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
     implementation("com.google.code.gson:gson:2.9.0")
 
     testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
