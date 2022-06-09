d30d1da1c65cab442f1493a53fc9198cc39c1307
DoTheBestMayB <send1684@gmail.com>
2022-06-09 20:14:20 +0900

    buid: data모듈에 retrofit2, gson 의존성 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -7,3 +7,8 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
+
+dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.google.code.gson:gson:2.9.0")
+}
